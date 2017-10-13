01dd081b8a4986f61e171fa99acf33d17c7b6a5b
Thomas Seropian <tseropian@gmail.com>
2017-10-13 11:35:47 +0100

    Create mac-setup.sh

---
+++ mac-setup.sh
@@ -0,0 +1,6 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew cask install atom
+brew cask install iterm2
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"

7aa48702cb9cb6984c1e3127ea7f5f78f92f477d
Thomas Seropian <tseropian@gmail.com>
2017-10-16 10:15:18 +0100

    Adding Google Drive

--- mac-setup.sh
+++ mac-setup.sh
@@ -2,5 +2,6 @@
 brew tap caskroom/cask
 brew cask install atom
 brew cask install iterm2
+brew cask install google-drive-file-stream
 brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
